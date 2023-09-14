6393105ab3471bd78dcf66ba721cd49750d86c7c
Braian Ledantes <braianledantes@gmail.com>
2023-09-14 12:58:29 -0300

    proband

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -32,6 +32,7 @@ export class DrinkController {
     }
 
     static async create(req, res) {
+        console.log("Controler", "create drink", req.body);
         const result = validateDrink(req.body);
 
         if (!result.success) {
